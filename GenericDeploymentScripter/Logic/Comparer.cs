e927d2efb1da9937fc8f0897728847a4c7399b44
Patrone Manuel <manuel.patrone@stud.bbbaden.ch>
2020-12-24 00:18:03 +0100

    added option to exclude objects

--- GenericDeploymentScripter/Logic/Comparer.cs
+++ GenericDeploymentScripter/Logic/Comparer.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
+using System.Data;
 using GenericDeploymentScripter.Models;
+using Microsoft.Data.SqlClient;
 using Microsoft.SqlServer.Dac.Compare;
 
 namespace GenericDeploymentScripter.Logic
@@ -17,12 +19,11 @@ namespace GenericDeploymentScripter.Logic
                 SchemaCompareDatabaseEndpoint target = new SchemaCompareDatabaseEndpoint(options.targetConnectionString);
 
                 var comparison = new SchemaComparison(source, target);
-                comparison.SaveToFile("testfile.txt", true);
+                //comparison.SaveToFile("testfile.txt", true);
                 comparison.Options = new Microsoft.SqlServer.Dac.DacDeployOptions() { AllowIncompatiblePlatform = true };
-
                 SchemaComparisonResult result = comparison.Compare();
                 var a = result.GetErrors();
-                //excludeObjects(result);
+                excludeObjects(result);
                 var differences = result.GenerateScript(target.DatabaseName);
                 string script = differences.Script;
             }
@@ -34,17 +35,57 @@ namespace GenericDeploymentScripter.Logic
 
         private void excludeObjects(SchemaComparisonResult results)
         {
+            List<string> objects = getListOfObjects();
+            int a = 0;
             foreach (SchemaDifference diff in results.Differences)
             {
+                Console.WriteLine(a);
                 string name = string.Join(".", (diff.SourceObject ?? diff.TargetObject).Name.Parts);
-                // if (postExcludeProp.Contains(name))
-                //{
-                results.Exclude(diff);
-                // }
+                if (!objects.Contains(name))
+                {
+                    results.Exclude(diff);
+                }
+                a++;
             }
+            Console.WriteLine("dd");
         }
 
-        //private void getListOf
-    }
+        private List<string> getListOfObjects()
+        {
+            List<string> objectNames = new List<string>();
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(options.sourceConnectionString))
+                {
+                    Console.WriteLine("\nQuery data example:");
+                    Console.WriteLine("=========================================\n");
 
+                    connection.Open();
+
+                    String sql = "SELECT SchemaName+'.'+ObjectName AS objectName FROM dbo.DDLChanges WHERE LoginName=@id";
+                    var cmd = new SqlCommand("SELECT SchemaName+'.'+ObjectName AS objectName FROM dbo.DDLChanges WHERE LoginName=@id");
+                    cmd.Parameters.Add("@id", SqlDbType.NVarChar).Value = options.username;
+                    cmd.Connection = connection;
+
+                    using (cmd)
+                    {
+                        using (SqlDataReader reader = cmd.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                Console.WriteLine("{0}", reader.GetString(0));
+                                objectNames.Add(reader.GetString(0));
+                            }
+                        }
+                    }
+                }
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            return objectNames;
+        }
+
+    }
 }
